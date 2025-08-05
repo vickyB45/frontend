4504d374c27b01a0e52b1cbc867a0e07fffc0cb4
vimal singh <v.bisht.unic@gmail.com>
2025-08-05 20:28:02 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBaseUrl = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://node-backend-2-dzae.onrender.com/api",
 });
 
 export const sendEnquery = async (value) => {
